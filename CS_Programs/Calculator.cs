c0b65bdf7c867455aa3fcc709538dfdecc8e73a3
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-03-09 14:01:58 +0545

    Added calculator functions

---
+++ CS_Programs/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programs
+{
+    class Calculator
+    {
+        public double Sum(double first, double second)
+        {
+            double sum = first + second;
+            return sum;
+        }
+        public double Difference(double first, double second)
+        {
+            double diff = first - second;
+            return diff;
+        }
+        public double Product(double first, double second)
+        {
+            double product = first * second;
+            return product;
+        }
+        public double Quotient(double first, double second)
+        {
+            double quotient = first / second;
+            return quotient;
+        }
+        public double Remainder(double first, double second)
+        {
+            double remains = first % second;
+            return remains;
+        }
+    }
+}
--- CS_Programs/Program.cs
+++ CS_Programs/Program.cs
@@ -11,17 +11,56 @@ namespace CS_Programs
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter first number: ");
-            int first = Convert.ToInt32(Console.ReadLine());
+            double first = 0, second = 0;
 
-            Console.Write("Enter second number: ");
-            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choices: " + "1, " + "2, " + "3, " + "4, " + "5");
 
-            CS_Programs.UserInput calculate = new UserInput();
-            int result = calculate.Sum(first, second);
+            Console.Write("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("The sum of two numbers are: " + result);
-            Console.WriteLine();
+            if(choice >= 1 && choice <=5)
+            {
+                Console.Write("Enter first number: ");
+                first = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                second = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if(choice == 1)
+            {
+                CS_Programs.Calculator calculate = new Calculator();
+                double result = calculate.Sum(first, second);
+                Console.WriteLine("The sum of two numbers are: " + result);
+            }
+            else if(choice == 2)
+            {
+                CS_Programs.Calculator calculate = new Calculator();
+                double result = calculate.Difference(first, second);
+                Console.WriteLine("The difference of two numbers are: " + result);
+            }
+            else if(choice == 3)
+            {
+                CS_Programs.Calculator calculate = new Calculator();
+                double result = calculate.Product(first, second);
+                Console.WriteLine("The product of two numbers are: " + result);
+            }
+            else if (choice == 4)
+            {
+                CS_Programs.Calculator calculate = new Calculator();
+                double result = calculate.Quotient(first, second);
+                Console.WriteLine("The quotient of two numbers are: " + result);
+            }
+            else if (choice == 5)
+            {
+                CS_Programs.Calculator calculate = new Calculator();
+                double result = calculate.Remainder(first, second);
+                Console.WriteLine("The remainder of two numbers are: " + result);
+            }
+            else
+            {
+                Console.WriteLine("No such choice available!");
+            }
 
             Console.ReadKey();
         }
