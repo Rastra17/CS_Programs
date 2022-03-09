fd50267bb1d7e437e62632e0e1ce306fe637f919
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-03-09 12:43:29 +0545

    Adding external programs for main function to execute

--- CS_Programs/Program.cs
+++ CS_Programs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CS_Programs;
 
 namespace CS_Programs
 {
@@ -10,7 +11,17 @@ namespace CS_Programs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.Write("Enter first number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            CS_Programs.UserInput calculate = new UserInput();
+            int result = calculate.Sum(first, second);
+
+            Console.Write("The sum of two numbers are: " + result);
+            Console.WriteLine();
 
             Console.ReadKey();
         }
---
+++ CS_Programs/UserInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programs
+{
+    class UserInput
+    {
+        public int Sum(int first, int second)
+        {
+            int sum = first + second;
+            return sum;
+        }
+    }
+}
