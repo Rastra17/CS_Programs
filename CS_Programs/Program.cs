9d8197c76cbfde77d99a3fa563546393dc0fedf6
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-03-14 13:45:38 +0545

    implementing the try/catch/finally block

--- CS_Programs/Program.cs
+++ CS_Programs/Program.cs
@@ -15,52 +15,69 @@ namespace CS_Programs
 
             Console.WriteLine("Choices: " + "\n1 - Addition, " + "\n2 - Substraction, " + "\n3 - Multiplication, " + "\n4 - Division, " + "\n5 - Remainder");
 
-            Console.Write("Enter your choice: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
 
-            if(choice >= 1 && choice <=5)
-            {
-                Console.Write("Enter first number: ");
-                first = Convert.ToInt32(Console.ReadLine());
+                if (choice >= 1 && choice <= 5)
+                {
+                    try
+                    {
+                        Console.Write("Enter first number: ");
+                        first = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Enter second number: ");
-                second = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter second number: ");
+                        second = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Please enter a number!");
+                        System.Environment.Exit(0);
+                    }
+                }
 
-            }
+                CS_Programs.Calculator calculate = new Calculator(first, second);
 
-            CS_Programs.Calculator calculate = new Calculator(first, second);
-
-            if (choice == 1)
-            {
-                double result = calculate.Sum();
-                Console.WriteLine("The sum of two numbers are: " + result);
+                if (choice == 1)
+                {
+                    double result = calculate.Sum();
+                    Console.WriteLine("The sum of two numbers are: " + result);
+                }
+                else if (choice == 2)
+                {
+                    double result = calculate.Difference();
+                    Console.WriteLine("The difference of two numbers are: " + result);
+                }
+                else if (choice == 3)
+                {
+                    double result = calculate.Product();
+                    Console.WriteLine("The product of two numbers are: " + result);
+                }
+                else if (choice == 4)
+                {
+                    double result = calculate.Quotient();
+                    Console.WriteLine("The quotient of two numbers are: " + result);
+                }
+                else if (choice == 5)
+                {
+                    double result = calculate.Remainder();
+                    Console.WriteLine("The remainder of two numbers are: " + result);
+                }
+                else
+                {
+                    Console.WriteLine("No such choice available!");
+                }
             }
-            else if(choice == 2)
+            catch(Exception)
             {
-                double result = calculate.Difference();
-                Console.WriteLine("The difference of two numbers are: " + result);
+                Console.WriteLine("Please enter the choice in a number!");
+                System.Environment.Exit(0);
             }
-            else if(choice == 3)
+            finally
             {
-                double result = calculate.Product();
-                Console.WriteLine("The product of two numbers are: " + result);
+                Console.ReadKey();
             }
-            else if (choice == 4)
-            {
-                double result = calculate.Quotient();
-                Console.WriteLine("The quotient of two numbers are: " + result);
-            }
-            else if (choice == 5)
-            {
-                double result = calculate.Remainder();
-                Console.WriteLine("The remainder of two numbers are: " + result);
-            }
-            else
-            {
-                Console.WriteLine("No such choice available!");
-            }
-
-            Console.ReadKey();
         }
     }
 }
